836a81e60cf912703f3c9e692ebe3056a7a21ce5
Cecilia Castillo <ana.castillo@xoom.com>
2015-07-31 15:02:35 -0600

    cambios

--- cvvcard.java
+++ cvvcard.java
@@ -4,7 +4,10 @@ class cvvcard{
 	{
       String cardNumber="4916496137058314";
       for(int i=0; i<cardNumber.length;i++){
+<<<<<<< HEAD
       	//Imprimir el codigo para probar
+=======
+>>>>>>> parent of 87d2b87... cambios
       		
       }
 	}
